68dda09cadd09951039a2ddd2e128e059aab5d73
kripashankar <kripariehu@gmail.com>
2020-07-16 15:48:42 +0200

    Ïnitial Health Check

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,4 +1,14 @@
 package org.xpdojo.bank;
 
 public class Account {
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    String state="New";
+
 }
--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -8,8 +8,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class AccountTest {
 
     @Test
-	@Disabled
-    public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
+    public void checkAccountHealth() {
+        Account a =new Account();
+        assertThat(a.getState()).isEqualTo("New");
     }
 }
